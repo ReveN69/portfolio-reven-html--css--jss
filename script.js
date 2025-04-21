39d6019798c0658a4160f26ecafeac7dc27c7ed9
ReveN <166753943+ReveN69@users.noreply.github.com>
2025-04-20 21:05:16 -0400

    added darkmode and light mode switch buttons

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-/* ADDING DARK MODE TOGGLE BUTTON*/
+// Dark mode
+const toggle = document.getElementById('theme-toggle');
 
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-theme');
+  toggle.checked = true;
+}
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+  }
+});
