5e508620b9a606a209c29872606808a323df6ae7
ReveN <166753943+ReveN69@users.noreply.github.com>
2025-04-22 21:35:03 -0400

    added a favicon (created with GIMP), hover effects for some btns, added linked copied script for drawings,  changed some colors for the dark mode to a more pleasing grey

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ toggle.addEventListener('change', () => {
     localStorage.setItem('theme', 'light');
   }
 });
+
+// Share script copy link
+
+async function copyArtLink(button) {
+  const url = button.dataset.url;
+  
+  try {
+    await navigator.clipboard.writeText(url);
+    
+    // Feedback visuel
+    button.innerHTML = '<i class="fas fa-check"></i> Copié !';
+    button.classList.add('copied');
+    
+    // Réinitialiser après 2 sec
+    setTimeout(() => {
+      button.innerHTML = '<i class="fas fa-share-alt"></i> Share';
+      button.classList.remove('copied');
+    }, 2000);
+    
+  } catch (err) {
+    button.textContent = 'Erreur :(';
+    console.error("Échec de la copie :", err);
+  }
+}
